4a45a25d687d0d00f6a7142ccda16d77de0bb5ea
Davix76 <30471850+Davix76@users.noreply.github.com>
2017-09-05 09:15:41 +0200

    david-Garcia

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,75 @@
+/*jshint esversion: 6 */
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.position = "";
+    this.direction = "up";
+    this.waitingList = [];
+    this.passengers = [];
+
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+    start() {
+      this.time = setInterval(() => this.update(), 1000);
+    }
+
+
+    stop() {
+      clearInterval(this.time);
+    }
+
+    update() {
+      this.log();
+  }
+
+
+
+  _passengersEnter(indexOf) {
+    this.passengers.push(this.requests[indexOf]);
+     this.requests.splice(indexOf,1); }
+
+  _passengersLeave(indexOf) {
+      this.servedPassengers.push(this.passengers[indexOf]);
+        this.passengers.splice(indexOf,1);
+
+   }
+
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+         this.position = "up";
+         this.floor++;
+         console.log(this.direction);
+
+   }
+
+ }
+
+   floorDown() {
+     if (this.floor > 0) {
+           this.position = "down";
+           this.floor--;
+         }
+   }
+
+  call(person) {
+
+    this.requests.push(this.waitingList,this.originFloor);
+
+
+
+   }
+
+  log() {
+    console.log("Direction: "+this.position+" | Floor: "+this.floor);
+    console.log(this.requests);
+    console.log(this.passengers);
+
+   }
 }
 
 module.exports = Elevator;
